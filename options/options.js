8a7635c50430a147c759d3b5f430ae8c6c745dfc
Crowdbotics <team@crowdbotics.com>
2021-08-07 02:34:03 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "purple_morning_29595Identifier",
+  url: "https://purple_morning_29595Identifier.botics.co",
+  api: "https://purple_morning_29595Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
